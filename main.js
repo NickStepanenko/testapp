0ea1b803c3b13de228f26069686427e7a4aa9aba
NickStepanenko <massaalonso@yandex.ru>
2015-06-07 19:07:59 +0800

    It almost works.

--- main.js
+++ main.js
@@ -4,33 +4,48 @@
 
 var data = { "name": "Commits Data", "commits": [{"commit":"808258b785829c0eaed965c9022da636f7630e00","author":"NickStepanenko <massaalonso@yandex.ru>","date":"Fri Jun 5 18:03:27 2015 +0800","comment":"Работа с функциями в JavaScript","content":[]},{"commit":"ddf1fdf4f5660fbff147a49ccfd8eb5edd49f548","author":"NickStepanenko <massaalonso@yandex.ru>","date":"Fri Jun 5 18:08:01 2015 +0800","comment":"Для начала определим две функции: первая будет принимать на вход два аргумента и возвращать их произведение, а вторая вычтет из первого аргумента сначала второй, а потом третий. Обе функции находятся в отдельных файлах. Так же не забываем подключить файлы в главном файле проекта — exec.js","content":[{"filename":"bin/exec.js","status":"Modified","changes":[{"changesStartLine":"0","changesCode":"+var applyContent = require(\"applyContent\");\n+var minusContent = require(\"minusContent\");\n+\n+console.log(applyContent(1, 5));\n+console.log(minusContent(16, 5, 8));\n\n"}]},{"filename":"lib/applyContent.js","status":"Modified","changes":[{"changesStartLine":"0","changesCode":"+module.exports = function (a, b) {\n+    return a*b;\n+};\n\n"}]},{"filename":"lib/minusContent.js","status":"Modified","changes":[{"changesStartLine":"0","changesCode":"+module.exports = function (a, b, c) {\n+    return (a - b - c);\n+};\n\n"}]}]},{"commit":"f387e44a0f8a36bf27afe74052380a6046997917","author":"NickStepanenko <massaalonso@yandex.ru>","date":"Fri Jun 5 18:10:30 2015 +0800","comment":"При запуске программы окно вывода указала нам на ошибку при вызове функций applyContent и minusContent. И это не удивительно, ведь мы забыли учесть вложенность файлов в нашем проекте. Функции require необходимо передать в качестве параметра полный путь от текущего файла до требуемого. Исправляем написанное и смотрим результат","content":[{"filename":"bin/exec.js","status":"Modified","changes":[{"changesStartLine":"1","changesCode":"-var applyContent = require(\"applyContent\");\n-var minusContent = require(\"minusContent\");\n+var applyContent = require(\"../lib/applyContent\");\n+var minusContent = require(\"../lib/minusContent\");\n \n console.log(applyContent(1, 5));\n console.log(minusContent(16, 5, 8));\n\n"}]}]},{"commit":"9c6b6a7cc41e1cf180ae85270eea683640766113","author":"NickStepanenko <massaalonso@yandex.ru>","date":"Fri Jun 5 18:12:57 2015 +0800","comment":"Теперь избавимся от фунции и файла applyContent, они нам более неинтересны. Вместо них поставим функцию, подобную minusContent, но работающую ровным счетом наоборот. Добавляем новый файл в exec.js и выводим в консоль результат сложения трёх чисел.","content":[{"filename":"bin/exec.js","status":"Modified","changes":[{"changesStartLine":"1","changesCode":"-var applyContent = require(\"../lib/applyContent\");\n+var addContent = require(\"../lib/addContent\");\n var minusContent = require(\"../lib/minusContent\");\n \n-console.log(applyContent(1, 5));\n+console.log(addContent(7, 2 ,4));\n console.log(minusContent(16, 5, 8));\n\n"}]},{"filename":"lib/addContent.js","status":"Created","changes":[{"changesStartLine":"0","changesCode":"+module.exports = function (a, b, c) {\n+    return (a + b + c);\n+};\n\n"}]},{"filename":"lib/applyContent.js","status":"Deleted","changes":[{"changesStartLine":"1","changesCode":"-module.exports = function (a, b) {\n-    return a*b;\n-};\n\n"}]}]},{"commit":"eb7b11a994367aec1921538a440e7745032e3da4","author":"NickStepanenko <massaalonso@yandex.ru>","date":"Sat Jun 6 11:33:06 2015 +0800","comment":"Помимо всего прочего, в качестве параметра функции мы можем передавать и другую функцию. Попробуем создать метод applyFunction, который принимает на вход два параметра, а затем вызывает второй параметр от первого. Как мы видим, функции в JavaScript весьма гибкая вещь. Это знание пригодиться нам в будущем.","content":[{"filename":"bin/exec.js","status":"Modified","changes":[{"changesStartLine":"1","changesCode":" var addContent = require(\"../lib/addContent\");\n var minusContent = require(\"../lib/minusContent\");\n+var applyFunction = require(\"../lib/applyFunction\");\n \n console.log(addContent(7, 2 ,4));\n-console.log(minusContent(16, 5, 8));\n+console.log(minusContent(16, 5, 8));\n+\n+console.log(applyFunction(5, function (a) {\n+    return a*a;\n+}));\n\n"}]},{"filename":"lib/applyFunction.js","status":"Created","changes":[{"changesStartLine":"0","changesCode":"+module.exports = function (a, func) {\n+    return func(a);\n+};\n\n"}]}]}] };
 
-/*var data = { "name": "Commits Data", "commits": [{"commit":"808258b785829c0eaed965c9022da636f7630e00","author":"NickStepanenko <massaalonso@yandex.ru>","date":"Fri Jun 5 18:03:27 2015 +0800","comment":"Работа с функциями в JavaScript","content":[]},{"commit":"ddf1fdf4f5660fbff147a49ccfd8eb5edd49f548","author":"NickStepanenko <massaalonso@yandex.ru>","date":"Fri Jun 5 18:08:01 2015 +0800","comment":"Для начала определим две функции: первая будет принимать на вход два аргумента и возвращать их произведение, а вторая вычтет из первого аргумента сначала второй, а потом третий. Обе функции находятся в отдельных файлах. Так же не забываем подключить файлы в главном файле проекта — exec.js","content":[{"filename":"bin/exec.js","status":"Modified","changes":[{"changesStartLine":"0","changesCode":"+var applyContent = require(\"applyContent\");\n+var minusContent = require(\"minusContent\");\n+\n+console.log(applyContent(1, 5));\n+console.log(minusContent(16, 5, 8));\n\n"}]},{"filename":"lib/applyContent.js","status":"Modified","changes":[{"changesStartLine":"0","changesCode":"+module.exports = function (a, b) {\n+    return a*b;\n+};\n\n"}]},{"filename":"lib/minusContent.js","status":"Modified","changes":[{"changesStartLine":"0","changesCode":"+module.exports = function (a, b, c) {\n+    return (a - b - c);\n+};\n\n"}]}]},{"commit":"f387e44a0f8a36bf27afe74052380a6046997917","author":"NickStepanenko <massaalonso@yandex.ru>","date":"Fri Jun 5 18:10:30 2015 +0800","comment":"При запуске программы окно вывода указала нам на ошибку при вызове функций applyContent и minusContent. И это не удивительно, ведь мы забыли учесть вложенность файлов в нашем проекте. Функции require необходимо передать в качестве параметра полный путь от текущего файла до требуемого. Исправляем написанное и смотрим результат","content":[{"filename":"bin/exec.js","status":"Modified","changes":[{"changesStartLine":"1","changesCode":"-var applyContent = require(\"applyContent\");\n-var minusContent = require(\"minusContent\");\n+var applyContent = require(\"../lib/applyContent\");\n+var minusContent = require(\"../lib/minusContent\");\n \n console.log(applyContent(1, 5));\n console.log(minusContent(16, 5, 8));\n\n"}]}]}] };*/
-
+var files = getFiles(data);
+var filesData = modifyData(files);
+console.log("================ This is what you get:");
+console.log(filesData);
 
 function getFiles() {
+    var filesData = data.commits;
     var commits = [];
     var filesAndCodes = [];
     var file = {};
 
-    for(i=1; i<data.commits.length; i++) {
-        for(j=0; j<data.commits[i].content.length; j++) {
-            file.filename = data.commits[i].content[j].filename;
-            file.status   = data.commits[i].content[j].status;
-            file.changes  = data.commits[i].content[j].changes;
+    for(i=1; i<filesData.length; i++) {
+        for(j=0; j<filesData[i].content.length; j++) {
+            file.filename = filesData[i].content[j].filename;
+            file.status   = filesData[i].content[j].status;
+            file.changes  = filesData[i].content[j].changes;
 
-            filesAndCodes[filesAndCodes.length] = file;
+            filesAndCodes.push(file);
             file = {};
         }
-        commits[commits.length] = filesAndCodes;
+        commits.push(filesAndCodes);
         filesAndCodes = [];
     }
     return commits;
 }
 
-var commitStages = [];
+function unmarkLines(code) {
+    var lines = code.split('\n');
+    newCode = "";
 
-function modifyData(filesData) {
+    for(i=0; i<lines.length; i++) {
+        lines[i] = " " + lines[i].slice(1);
+        newCode += lines[i] + "\n";
+    }
+
+    return newCode;
+}
+
+function modifyData(data) {
+    var filesData = data;
     var currentStage = [];
+    var commitStages = [];
     var count_i = 0;
 
     while(count_i < filesData.length) {
@@ -38,23 +53,22 @@ function modifyData(filesData) {
             var j = 0;
             for(j=0; j<filesData[count_i].length; j++) {
                 filesData[count_i][j].code = getCode(filesData[count_i][j].changes);
-                currentStage[currentStage.length] = filesData[count_i][j];
+                filesData[count_i][j].code = unmarkLines(filesData[count_i][j].code);
+                currentStage.push(filesData[count_i][j]);
             }
             j=0;
 
-            commitStages[commitStages.length] = currentStage;
+            commitStages.push(currentStage);
             currentStage = [];
         }
         else {
-            if(count_i == 3) {
-                var counter = 3;
-            }
             currentStage = commitStages[count_i-1];
 
             var m = 0;
             for(m=0; m<currentStage.length; m++) {
                 if(currentStage[m].status != "Deleted") {
                     currentStage[m].status = "Default";
+                    currentStage[m].code = unmarkLines(currentStage[m].code);
                 }
             }
             m = 0;
@@ -72,14 +86,14 @@ function modifyData(filesData) {
                     }
                     k = 0;
                 }
-                else
 
+                else
                 if(filesData[count_i][l].status == "Created") {
                     filesData[count_i][l].code = getCode(filesData[count_i][l].changes);
-                    currentStage[currentStage.length] = filesData[count_i][l];
+                    currentStage.push(filesData[count_i][l]);
                 }
-                else
 
+                else
                 if(filesData[count_i][l].status == "Deleted") {
                     var p = 0;
                     for(p=0; p<currentStage.length; p++) {
@@ -91,9 +105,9 @@ function modifyData(filesData) {
                 }
             }
             l = 0;
-            commitStages[commitStages.length] = currentStage;
-            currentStage = [];
+            commitStages.push(currentStage);
         }
+        currentStage = [];
         count_i++;
     }
 
@@ -118,18 +132,19 @@ function applyPatch(code, patches) {
         var count_k = (patches[i].changesCode.split('\n')).length;
 
         while(count_j < patches[i].changesStartLine-1) {
-            part1[part1.length] = code[count_j];
+            part1.push(code[count_j]);
             count_j++;
         }
-        codeArray[codeArray.length] = part1;
+
+        codeArray.push(part1);
 
         while(count_k < code.length) {
-            part2[part2.length] = code[k];
+            part1.push(code[k]);
             count_k++;
         }
-        codeArray[codeArray.length] = patches[i].changesCode.split('\n');
 
-        codeArray[codeArray.length] = part2;
+        codeArray.push(patches[i].changesCode.split('\n'));
+        codeArray.push(part2);
 
         for(j=0; j<codeArray.length; j++) {
             for(k=0; k<codeArray[j].length; k++) {
@@ -140,5 +155,3 @@ function applyPatch(code, patches) {
 
     return finalCode;
 }
-var filesData = modifyData(getFiles(data));
-console.log(filesData);
